8d1ce0e5ffb5edcf29e9c6e27970cdf5099a663a
irons163 <irons@ironsdeMacBook-Air.local>
2015-06-30 16:46:12 +0800

    Initial Commit

---
+++ Try_Money_Dig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Try_Money_Dig
+//
+//  Created by irons on 2015/6/30.
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
